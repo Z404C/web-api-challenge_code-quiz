0a5c95a19c0ceb66acb049e31b50df064a23100b
Z404C <112136706+Z404C@users.noreply.github.com>
2022-11-04 17:47:25 -0400

    added to HTML and commenting JS file

---
+++ codequiz.js
@@ -0,0 +1,36 @@
+//GOAL: Create timed quiz that stores high scores.
+
+//variables
+
+
+//functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//When start button clicked, timer starts, and a question shows
+
+//When a question is answered, a different question shows 
+
+//if A question is answered wrong, then time is subtracted from timer
+
+//When all questions are answered OR when the time reaches 0, show game over screen and score.
+
+//With game over screen, an option to write intitals should show and submit score should show.
