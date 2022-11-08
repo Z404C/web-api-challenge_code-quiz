4630d720eb7af9477a20cbd40463d8728c81a374
Z404C <112136706+Z404C@users.noreply.github.com>
2022-11-07 23:19:30 -0500

    added buttons for choices

--- codequiz.js
+++ codequiz.js
@@ -1,6 +1,23 @@
 //GOAL: Create timed quiz that stores high scores.
 
 //variables
+var questions;
+var timer; 
+var isCorrect;
+var highscores;
+var timerCount;
+
+//create variables for elements 
+var startButton = document.querySelector("#start");
+var questionsEl = document.querySelector("#questions");
+var endScreenEl = document.querySelector("#end-screen");
+var quizAreaEl = document.querySelector(".quiz-area");
+var highscoreEl = document.querySelector("#highscore");
+var timerEl = document.querySelector("#timer");
+
+
+
+
 
 
 //functions
@@ -9,23 +26,40 @@
 
 
 
+function clock(){
+    
+    timer=setInterval(function(){
+        timerCount--;
+        timerElement.textContent=timerCount;
+        if(timerCount>=0){
+            if(isCorrect && timerCount> 0){
+                clearInterval(timer);
+            }
+        }
+
+        if (timerCount===0){
+            clearInterval(timer);
+        }
+
+    },1000)
+};
+
+
+function startGame(){
+    
+    timerCount = 60;
+}
 
 
 
-
-
-
-
-
-
-
-
-
+//get id "questions" element to insert questions into it.
 
 
 
 
 //When start button clicked, timer starts, and a question shows
+startButton.addEventListener("click", startGame);
+
 
 //When a question is answered, a different question shows 
 
